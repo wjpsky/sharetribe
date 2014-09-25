bd0006b011ee205e62f92a5d81c802612f794208
Mikko Koski <mikkokos@gmail.com>
2014-09-25 16:29:47 +0300

    Add bool and time transformers

--- app/utils/entity_utils.rb
+++ app/utils/entity_utils.rb
@@ -93,6 +93,9 @@ module EntityUtils
     const_value: -> (const, v) { const },
     default: -> (default, v) { v.nil? ? default : v },
     bool: -> (_, v) { !!v },
+    to_bool: -> (_, v) { !!v },
+    int_to_bool: -> (_, v) { v.present? && v != 0 },
+    str_to_time: -> (_, v) { v.is_a?(Time) ? v : Time.parse(v) },
     transform_with: -> (transformer, v) { transformer.call(v) }
   }
 
